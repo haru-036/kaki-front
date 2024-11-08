83340a0574b0c8363f1f4e14e70e5bd9a6bf3311
haru <hinagoro5656@gmail.com>
2024-11-08 19:17:37 +0900

    feat: HomeのUI作成

---
+++ components/NoticeCard.tsx
@@ -0,0 +1,35 @@
+import { CalendarDays } from "lucide-react";
+import { Card, CardHeader, CardContent } from "./ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+const NoticeCard = () => {
+  return (
+    <Card className="max-w-lg">
+      <CardHeader className="pb-4">
+        <div className="flex items-center gap-2">
+          <Avatar className="w-7 h-7">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <h4 className="text-sm font-semibold">username</h4>
+          <p className="text-muted-foreground text-sm font-normal">
+            commented to プロジェクト名
+          </p>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <p className="text-sm">
+          コメント内容コメント内容コメント内容コメント内容コメント内容コメント内容コメント内容コメント内容コメント内容コメント内容コメント内容
+        </p>
+        <div className="flex items-center pt-3">
+          <CalendarDays className="mr-2 h-4 w-4 opacity-70" />
+          <span className="text-xs text-muted-foreground">
+            December 7, 2021
+          </span>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default NoticeCard;
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 const Header = () => {
   return (
     <>
-      <div className="flex items-center justify-between px-6 py-4 bg-secondary">
+      <div className="flex items-center justify-between px-6 py-4 border-b">
         <Link href={"/"} className="text-lg font-semibold">
-          イラスト版GitHub {/* ロゴにしたい */}
+          <Avatar className="bg-white">
+            <AvatarImage src="/logo.png" />
+            <AvatarFallback>LOGO</AvatarFallback>
+          </Avatar>
         </Link>
 
         {/* 一意のユーザー名をリンクにする */}
