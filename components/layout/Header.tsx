b38c8706b4f20e0d9aee7c79f0765c919b547320
haru <hinagoro5656@gmail.com>
2024-11-17 15:02:07 +0900

    fix

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,7 +2,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
 import { Button } from "../ui/button";
-import NotificationPopover from "../NotificationPopover";
 import { useContext } from "react";
 import { AuthContext } from "../AuthProvider";
 import UserIcon from "../UserIcon";
